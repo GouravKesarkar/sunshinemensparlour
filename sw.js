af775dc13dfa0263dc227e6d355079abe0d3ab1b
GouravKesarkar <gourav.anand.kesarkar@gmail.com>
2025-10-14 15:44:20 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "gentle-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon/mylibrary-icon-192.png"
+];
+
+// Install event - cache assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+// Fetch event - serve from cache first
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
